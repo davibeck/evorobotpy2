023e12300d1203c967dd4a028ee5e6e15e8dd28e
Davi lab <davi.becker@hotmail.com>
2023-06-29 09:32:45 -0300

    adding new results

---
+++ results/halfcheetah/2_50/boxsplot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+davi_2n_50g = [2232.16, 1471.19, 2508.74, 1381.31, 2662.64, 15.91  ,351.30 , 2249.77, 2655.73, 944.21]
+brenda_2n_50g = [1692.05, 356.32 , 1288.87, 840.49 , 1753.54, 2204.37, 1300.69, 624.32 , 1392.67, 1085.66]
+larissa_500 = [1982.69, 2235.67, 2333.56, 2524.10, 684.58 , 1561.83, 1975.81, 1664.63, 1221.70, 392.11 ]
+
+
+data = [davi_2n_50g,  brenda_2n_50g, larissa_500]
+
+fig, ax = plt.subplots()
+ax.boxplot(data)
+
+ax.set_xticklabels(['Davi 2-50', 'Brenda 2-50', 'Larissa 500'])
+ax.set_ylabel('Valores Fitness')
+
+plt.title('Boxplot - OpenAi-Es vs OpenAi-Es-Ne')
+#plt.figure(figsize=)
+plt.savefig('2_50.png')
+#plt.show()
